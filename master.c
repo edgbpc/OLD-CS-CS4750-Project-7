8dff8f9d82100884b34917809271b92d2ec81aa6
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-02-14 16:55:06 -0600

    Initial commit of master.c - termination after 100 secs implemmented, can allocate shared memory, can store turn variable in shared memory, can call consumer and execute in a child process

---
+++ master.c
@@ -0,0 +1,124 @@
+//Author: Eric Goodwin
+//Project 2 - Current Unix Processes
+//Class: OS, Evening section
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+
+#define SHM_SIZE 1024
+
+/*
+void print_usage(){
+printf("stuff");
+}
+
+int spawnProcesses(const char * program, char ** arg_list){
+	pid_t child_pid = fork();
+	if (child_pid != 0) {
+		return child_pid; //[arent process pid
+	} else {
+		execvp (program, arg_list);
+		fprintf (stderr, "An error occured in execvp\n");
+		abort();
+	}
+}
+*/
+void handle(int sig){
+	printf("Terminating program");
+	exit(0);
+}
+
+int main (int argc, char *argv[]){
+	
+
+//set alarm  timer to 100 seconds.  signal invokes SIGALRM and calls handle to terminate program
+alarm(100);
+signal(SIGALRM, handle);
+
+key_t key = 59566;
+char *data;
+int mode;
+
+int option;
+int numConsumers;
+
+//int fd[2];
+//int producerID = 0;
+
+char producerDesc[20] = "Producer";
+char consumerDesc[20] = "Consumer";
+char readDesc[20];
+
+//int childID = 1;
+
+int shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0777);
+
+
+while ((option = getopt(argc, argv, "n:h")) != -1){
+	switch (option){
+		case 'n' : numConsumers = atoi(optarg);
+			break;
+
+		case 'h' : // print_usage();
+			return (0);
+			break;
+
+	default : //print_usage();
+		exit(EXIT_FAILURE);
+	}
+}
+
+char * paddr = (char*)(shmat(shmid, 0, 0));
+int * turn = (int *)(paddr);
+
+*turn = 5;
+
+pid_t childpid = fork();
+
+if (childpid == 0){
+printf("here!\n");
+execv("./consumer", NULL);
+}
+
+//printf("%d", turn);
+
+/* Code is just to check if parent was sending data to child and child could read it
+
+pipe(fd);
+
+if (fork() !=0) {
+
+close (fd[0]);
+
+write(fd[1], producerDesc, strlen(producerDesc)+1);
+write(fd[1], &numConsumers, sizeof(childID));
+printf("Parent(%d): There are  %d number of consumers\n", getpid(), numConsumers);
+printf("Parent(%d): Child's description is %s\n", getpid(), producerDesc);
+close(fd[1]);
+
+} else {
+
+close(fd[1]);
+
+read(fd[0], &childID, sizeof(childID));
+read(fd[0], producerDesc, strlen(producerDesc)+1);
+printf("Child(%d): There are %d number of consumers\n", getpid(), numConsumers);
+printf("Child(%d): I am a %s\n", getpid(), producerDesc);
+close(fd[0]);
+}
+*/
+	return 0;
+
+
+
+}	
