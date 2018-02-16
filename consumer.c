ed2204028085df9a8836d51efca9f0d7035bafd6
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-02-16 15:00:29 -0600

    solved problem with reading structs in shared memory'

--- consumer.c
+++ consumer.c
@@ -12,25 +12,47 @@
 #include <time.h>
 #include <string.h>
 
-#define SHM_SIZE 1024
+#define SHM_SIZE 1000
+#define BUFFERSIZE 50
+
+typedef struct {
+	char buffer[BUFFERSIZE];
+	int flag;
+}bufferType;
 
 int main (int argc, char *argv[]){
 
-int *turn;
-int *flag;
+bufferType bufferOne, *bufferOnePtr;
 
-printf("From consumer child\n");
 
-key_t key = 59566;
+key_t keyTurn = 59566;
 
-int shmid = shmget(key, SHM_SIZE, 0777);
+key_t keyBufferOne = 59567;
+key_t keyBufferTwo = 59568;
+key_t keyBufferThree = 59569;
+key_t keyBufferFour = 59560;
+key_t keyBufferFive = 59561;
 
-turn = (int *)(shmat (shmid, 0, 0 ));
-flag = turn +1;
 
-printf("%d\n", *turn);
+int shmidTurn = shmget(keyTurn, SHM_SIZE, 0777);
+
+int shmidBufferOne = shmget(keyBufferOne, SHM_SIZE, 0777);
+//int shmidBufferTwo = shmget(keyBufferTwo, SHM_SIZE, 0777);
+//int shmidBufferThree = shmget(keyBufferThree, SHM_SIZ, 0777);
+//int shmidBufferFour = shmget(keyBufferFour, SHM_SIZE, 0777);
+//int shmidBufferFive = shmget(keyBufferFive, SHM_SIZE, 0777);
+
+int * turn = (int *) (shmat(shmidTurn, 0, 0));
+
+bufferOnePtr = (bufferType *)(shmat (shmidBufferOne, 0, 0));
+
+
+//bufferOnePtr = &bufferOne;
+
+printf("In Child - Flag value is %s :::::\n ", bufferOnePtr->buffer); 
+printf("In Child - Turn: %d :::::::::::::\n ", *turn);
+
 
 return 1;
-
 }
 
