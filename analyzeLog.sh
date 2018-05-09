bb39336368050b395a8075f2fe0691552e342a58
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-05-08 20:51:57 -0500

    need to add number of attempts to enter critical section

--- analyzeLog.sh
+++ analyzeLog.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 checks=$(grep "Check" $1 | wc -l)  
-
 echo "$checks checks  made in $1"
 
 
+totalSleep=$(grep "Sleep" $1 | awk '{ SUM += $NF} END { print SUM }')
+echo "$totalSleep sleep time in $1"
 
+
+numWrites=$(grep "Write" $1 | wc -l)
+echo "$numWrites writes in $1"
+
+numReads=$(grep "Reads" $1 | wc -l)
+echo "$numReads reads in $1"
